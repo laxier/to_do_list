0b348e8b3131352b091b915ac3224ff94b84f50e
Кирилл Т <hash0xcb8c@yandex.ru>
2024-09-23 21:21:25 +0300

    Create views TaskDelete, TaskEdit instead of TaskDetail

--- todolist_project/homepage/urls.py
+++ todolist_project/homepage/urls.py
@@ -6,5 +6,7 @@ app_name = 'homepage'
 
 urlpatterns = [
     path('', views.TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', views.TaskDetail.as_view(), name='detail')
+    path('task-update/<int:pk>/', views.TaskEdit.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', views.TaskDelete.as_view(), name='task-delete'),
+    path('task-create/', views.TaskCreate.as_view(), name='task-create'),
 ]
--- todolist_project/homepage/views.py
+++ todolist_project/homepage/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Task
+
+
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
 
-class TaskDetail(DetailView):
+
+class TaskEdit(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('homepage:tasks')
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('homepage:tasks')
+
+class TaskDelete(DeleteView):
     model = Task
     context_object_name = 'task'
-    template_name = 'homepage/task.html'
+    success_url = reverse_lazy('homepage:tasks')
