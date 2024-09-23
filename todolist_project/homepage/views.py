98781768b7700f3338fc1400481501d79aaa034e
Кирилл Т <hash0xcb8c@yandex.ru>
2024-09-23 22:16:41 +0300

    Automatic form.instance.user = self.request.user

--- todolist_project/homepage/views.py
+++ todolist_project/homepage/views.py
@@ -10,19 +10,27 @@ class RestrictedMixin(LoginRequiredMixin):
 
 class FormMixin:
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'completed']
     success_url = reverse_lazy('homepage:tasks')
 
 
 class TaskList(RestrictedMixin, ListView):
     model = Task
     context_object_name = 'tasks'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(completed=False).count()
+        return context
 
 class TaskEdit(RestrictedMixin, FormMixin, UpdateView):
     pass
 
 class TaskCreate(RestrictedMixin, FormMixin, CreateView):
-    pass
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
 
 class TaskDelete(RestrictedMixin, FormMixin, DeleteView):
     context_object_name = 'task'
